dafaa0897e5dbd17ea59bcc2e28575c5dfb79fec
Gustavo Ferreira da Silva <gusta23clash@gmail.com>
2023-02-05 19:13:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var nome = prompt("Digite seu nome: ");
+var valorReal = prompt("Digite o valor em reais para ser convertido: ");
+var valorDoDolar = 0.2;
+var valorDoEuro = 0.18;
+var valorDoBitcoin = 0.0000085;
+
+var valorEmDolar = valorReal * valorDoDolar;
+var valorEmEuro = valorReal * valorDoEuro;
+var valorEmBitcoin = valorReal * valorDoBitcoin;
+
+alert(
+    "Olá " +
+    nome +
+    "! Aqui está o resultado: " +
+    valorEmDolar.toFixed(2) +
+    " Dólares / " +
+    valorEmEuro.toFixed(2) +
+    " Euros / " +
+    valorEmBitcoin +
+    " BTC"
+);
